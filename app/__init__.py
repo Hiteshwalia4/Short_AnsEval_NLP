97da5c7a1dd0523758fd6e07e79cd91ce034a278
Hitesh <hiteshwalia.194071@sggscc.ac.in>
2023-10-21 20:14:19 +0530

    Initial Work

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-#Flask
-from flask import Flask
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'thisisasecretkey'
-from app import routes
-
--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class AnswerForm(FlaskForm):
-    answer1 = StringField(label="Answer 1:", validators=[DataRequired()])
-    answer2 = StringField(label="Answer 2:", validators=[DataRequired()])
-    answer3 = StringField(label="Answer 3:", validators=[DataRequired()])
-    submit = SubmitField(label="Submit")
