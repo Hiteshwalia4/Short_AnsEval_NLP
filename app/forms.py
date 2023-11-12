c6b1f36ea740b4a74d3c4e42132b4eb52cf77fcd
Hitesh <hiteshwalia.194071@sggscc.ac.in>
2023-11-13 00:23:45 +0530

    Added ValidationError

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
 
 class RegisterForm(FlaskForm):
     username=StringField(label="User name", validators=[DataRequired(), Length(min=2,max=25)])
@@ -8,7 +9,17 @@ class RegisterForm(FlaskForm):
     password1= PasswordField(label="Password", validators=[DataRequired(), Length(min=6)])
     password2= PasswordField(label="Confirm Password", validators=[DataRequired(), EqualTo('password1')])
     submit=SubmitField(label="Create Account")
-
+     
+    def validate_username(self, username_to_check):
+       user=User.query.filter_by(username=username_to_check.data).first()
+       if user:
+           raise ValidationError("Username already exists. Please try a different user.")
+    
+    def validate_email_address(self, email_to_check):
+       email=User.query.filter_by(email_address=email_to_check.data).first()
+       if email:
+           raise ValidationError("Email Id already exists, try a different Email-Id.")
+          
 
 class LoginForm(FlaskForm):
     username=StringField(label="User Name", validators=[DataRequired()])    
