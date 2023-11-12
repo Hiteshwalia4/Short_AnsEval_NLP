1095ad5cc29256d56c9d4efa9e97effdd818ad21
Hitesh <hiteshwalia.194071@sggscc.ac.in>
2023-11-12 14:46:18 +0530

    Added Register, Login Classes

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegisterForm(FlaskForm):
+    username=StringField(label="User name", validators=[DataRequired(), Length(min=2,max=25)])
+    email_address=StringField(label="E-mail Address", validators=[DataRequired(),Email()])
+    password1= PasswordField(label="Password", validators=[DataRequired(), Length(min=6)])
+    password2= PasswordField(label="Confirm Password", validators=[DataRequired(), EqualTo('password1')])
+    submit=SubmitField(label="Create Account")
+
+
+class LoginForm(FlaskForm):
+    username=StringField(label="User Name", validators=[DataRequired()])    
+    password=PasswordField(label="Password", validators=[DataRequired()])   
+    submit=SubmitField(label="Sign In") 
+
 
 class AnswerForm(FlaskForm):
     answer1 = StringField(label="Answer 1:", validators=[DataRequired()])
