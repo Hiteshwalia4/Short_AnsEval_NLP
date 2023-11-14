1536ab642b4391644a967795fb42e5a6f20e8104
Hitesh <hiteshwalia.194071@sggscc.ac.in>
2023-11-14 18:02:20 +0530

    Updated init

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,10 @@ app.config['SECRET_KEY'] = 'thisisasecretkey'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///users.db'
 
 db=SQLAlchemy()
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
 bcrypt= Bcrypt(app)
 login_manager=LoginManager(app)
 login_manager.login_view="login_page"
