90b3322b240ff80f09933def3510cf8a306b095f
Hitesh Walia <81966053+Hiteshwalia4@users.noreply.github.com>
2024-09-22 14:16:17 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(db.Model, UserMixin()):
+class User(UserMixin, db.Model):
     id=db.Column(db.Integer(), primary_key=True)
     username=db.Column(db.String(length=25), unique=True, nullable=False)
     email_address= db.Column(db.String(length=50), unique=True, nullable=False)
